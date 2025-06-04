74b36f90b6cd1a7116eed92817448c4862c13972
fhamyla <fhamyla.devera@gmail.com>
2025-06-04 13:57:07 +0800

    Estimated value of pi after 1000, 10000, 100000

--- Laboratory 06/main.py
+++ Laboratory 06/main.py
@@ -6,8 +6,8 @@ def estimate_pi(n_trials):
         x, y = random.uniform(0, 1), random.uniform(0, 1)
         if x**2 + y**2 <= 1:
             inside_circle += 1
-            return 4 * inside_circle / n_trials
+    return 4 * inside_circle / n_trials
 
-num_trials = 100000
-pi_estimate = estimate_pi(num_trials)
-print(f"Estimate value of n after {num_trials} trials: (pi_estimate)")
+for num_trials in [1000, 10000, 100000]:
+    pi_estimate = estimate_pi(num_trials)
+    print(f"Estimated value of pi after {num_trials} trials: {pi_estimate}")
